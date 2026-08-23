04888eba93f83241d1ccda083d6ef5374d738369
Akshatjain5868 <74894637+Akshatjain5868@users.noreply.github.com>
2026-08-24 02:45:45 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first=1,second=2;
+        if(n==1) return first;
+        if(n==2) return second;
+        for(int i=3; i<=n; i++){
+            int curr = first+second;
+            first = second;
+            second = curr;
+        }
+        return second;
+    }
+};
